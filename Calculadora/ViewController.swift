9aaa5c481c4aa6b6c6b514f3af0b3bf1c07d611a
Santiago <santiago.falcon@col.vurling.com>
2022-11-08 16:38:30 +0100

    calculator simple

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -13,7 +13,45 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+   
+    
+    @IBOutlet weak var labelTxt: UILabel!
+    
+    @IBOutlet weak var firstVal: UITextField!
+    
+    @IBOutlet weak var secondVal: UITextField!
+    
+    @IBAction func sum(_ sender: Any) {
+        labelTxt.text = String(sumar())
+    }
+    @IBAction func rest(_ sender: Any) {
+        labelTxt.text = String(resta())
+    }
+    
+    func sumar() -> Int {
+        guard
+        let myFirstVal = firstVal.text,
+        let mySecondVal = secondVal.text,
+        let myInt1 = Int(myFirstVal),
+        let myInt2 = Int(mySecondVal)
+        else { return 0 }
+        let resul = myInt1 + myInt2
+        return resul
+    
+    }
+    
+    func resta() -> Int {
+        guard
+        let myFirstVal = firstVal.text,
+        let mySecondVal = secondVal.text,
+        let myInt1 = Int(myFirstVal),
+        let myInt2 = Int(mySecondVal)
+        else { return 0 }
+        let resul = myInt1 - myInt2
+        return resul
+    
+    }
+   
 }
 
